e6a1eb18e139312325a50105b055ff1e31b82c6f
MrPluto <457808167@qq.com>
2016-08-04 23:32:34 +0800

    quick glance on electron

---
+++ elementary/index.js
@@ -0,0 +1,5 @@
+var picture = require('cat-picture')
+var image = require('lightning-image-poly')
+var src = picture.src;
+picture.remove();
+var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'})
