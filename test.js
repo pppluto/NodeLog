5017257ddd21b0d1442895eeb00fc303403e66da
MrPluto <457808167@qq.com>
2016-10-13 23:12:23 +0800

    continue

--- test.js
+++ test.js
@@ -1,11 +1,4 @@
-var mongoose = require('mongoose');
-
-var db = mongoose.connect('mongodb://localhost/test');
-
-db.connection.on('error', function(){
-  console.log('connection error');
-});
-
-db.connection.on('open', function(){
-  console.log('connection successful');
-});
+var birds = [1,2,3,4,5];
+var shots = [4,5,6,7,8];
+var a = [1,3,6,7,8,9].filter((c) => !birds.some((b) => c === b) &&!shots.some((s) => c === s))
+console.log('a',a);
