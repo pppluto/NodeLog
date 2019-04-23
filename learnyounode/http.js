081695b7be1dcef367b13850b6f7cf81ccdf518c
MrPluto <457808167@qq.com>
2019-04-23 16:06:40 +0800

    http simple

--- learnyounode/http.js
+++ learnyounode/http.js
@@ -1,18 +1,45 @@
 var http = require('http');
-
-var rUrl = process.argv[2];
+var bl = require('bl');
+var [a, b, ...rUrls] = process.argv;
 
 //Steam
-http.get(rUrl, response => {
-  response.setEncoding('utf8');
-  response.on('data', data => {
-    console.log(data);
-  });
+//pipe
+//异步顺序
+var totalData = [];
+var count = 0;
 
-  response.on('error', err => {
-    console.log(err);
+function doGet(url, index) {
+  http.get(url, response => {
+    response.setEncoding('utf8');
+
+    response.pipe(
+      bl((err, data) => {
+        if (err) throw err;
+
+        let str = data.toString();
+        totalData[index] = str;
+        // console.log(str.length);
+        // console.log(str);
+      })
+    );
+    // response.on('data', data => {
+    //   console.log(data);
+    // });
+
+    response.on('error', err => {
+      console.log(err);
+    });
+    response.on('end', err => {
+      count++;
+      if (count === rUrls.length) {
+        totalData.forEach(f => {
+          console.log(f);
+        });
+      }
+    });
   });
-  //   response.on('end', err => {
-  //     console.log('');
-  //   });
+}
+
+rUrls.map((url, index) => {
+  doGet(url, index);
 });
